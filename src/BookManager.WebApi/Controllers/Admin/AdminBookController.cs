757b88a7c195ef3a413f7e5c689248b9b69e8043
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-12-27 18:08:12 -0300

    Starting controller configuration

---
+++ src/BookManager.WebApi/Controllers/Admin/AdminBookController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BookManager.Application.Services;
+using BookManager.Application.ViewModels;
+using BookManager.Core.Communication.MediatR;
+using BookManager.Core.DomainObjects.ValueObjects;
+using BookManager.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManager.WebApi.Controllers.Admin
+{
+    public class AdminBookController : ApiController
+    {
+        private readonly IBookAppService _bookAppService;
+
+        public AdminBookController(IBookAppService bookAppService, INotificationHandler<DomainNotification> notifications,
+            IMediatrHandler mediatr) : base(notifications, mediatr)
+        {
+            _bookAppService = bookAppService;
+        }
+    }
+}
